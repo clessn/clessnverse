01613d28025da52dc9b15bad463c01cac9027ded
PatrickPoncet <patrick@poncet.ca>
2021-02-12 07:05:11 -0500

    Added agora datasets commit functions

--- R/etl.R
+++ R/etl.R
@@ -47,12 +47,107 @@ translateText <- function (text, engine = "azure", target_lang = "fr", fake = TR
 
       return(response_json[1][[1]]$translations[[1]]$text)
     }
-
   } else {
-
     stop("google translation not implemented")
-
   } # if (engine == "azure")
 
   return("Fake translation text - use fake = false if you want to consume the translation service")
+}
+
+
+######################################################
+#' @title clessnverse::commitToHub
+#' @description Writes the observation of a dataframe to the CLESSN Hub.  Updates it if uuid already exists and writeMode = "update"
+#' @param df : the obesrvation of the dataframe to write as a 1 row dataframe
+#' @param uuid : the uuid of the observation to write
+#' @param table : name of the table to be updated
+#' @param writeMode : same as SQL - "insert" | "update"
+#' @return
+#' @examples example
+#'
+#'
+#' @export
+commitToHub <- function (df, uuid, table, writeMode = "insert") {
+
+}
+
+######################################################
+#' @title clessnverse::deleteFromHub
+#' @description Deletes an observation from the CLESSN Hub based on its uuid.
+#' @param uuid : the uuid of the observation to delete
+#' @param table : name of the table to delete the observation from
+#' @return
+#' @examples example
+#'
+#'
+#' @export
+deleteFromHub <- function (uuid, table) {
+
+}
+
+
+######################################################
+#' @title clessnverse::commitAgoraInterventions
+#' @description adds, inserts or deletes an observation to the Agora hub
+#' @param df : the dataframe containing rows to be committed to the hub & local dataframe
+#' @param table : the name of the table the data must be commited to
+#' @param modeLocalData : "skip": totally skip the transaction
+#'                        "update" : update the dataset with new observations only
+#'                        "refresh" : updates the dataset with new observations and updates existing observations also
+#'                        "rebuild" : rebuilds the local dataset from scratch at the execution of the script & refresh the hub data
+#' @param modeHub : "skip"| "update" | "refresh" | "rebuild"
+#' @return the updated dataframe
+#' @examples example
+#'
+#'
+#' @export
+commitAgoraInterventions <- function (dfSource, dfDestination, hubTableName, modeLocalData = "skip", modeHub = "skip") {
+  # Primary key for interventions are eventID + interventionSeqNum
+  # use the fonction commitToHub
+
+  # Let's handle the local data first
+  for (i in 1:nrow(dfSource)) {
+    current_event_id <- dfSource$eventID[i]
+    current_seqnum <- dfSource$eventID[i]
+
+    if ( modeLocalData != "skip"  &&
+         nrow(dplyr::filter(dfDestination, eventID == current_event_id & interventionSeqNum == current_seqnum)) == 0) {
+      # If the eventID+interventionSeqNum does not already exist
+      # append it to the dataset
+      dfDestination <- dfDeep %>% rbind(dfSource[i,])
+
+      if ( modeHub != "skip" && dfSource$uuid == "") {
+        hub_row <- dfSource[i,] %>%
+          mutate_if(is.numeric , replace_na, replace = 0) %>%
+          mutate_if(is.character , replace_na, replace = "") %>%
+          mutate_if(is.logical , replace_na, replace = 0)
+
+        clessnhub::create_item(as.list(hub_row), hubTableName)
+      }
+    }
+
+
+    if ( modeLocalData == "refresh" &&
+         nrow(dplyr::filter(dfDestination, eventID == current_event_id & interventionSeqNum == current_seqnum)) > 0) {
+      # If the eventID+interventionSeqNum already exists and update_mode is "refresh"
+      # Update the existing row with primary key eventID*interventionSeqNum
+      matching_row_index <- which(dfDestination$eventID == current_id &
+                                  dfDestination$interventionSeqNum == current_seqnum)
+
+      dfDestination[matching_row_index,] <- dfSource[i,]
+
+      if ( modeHub == "refresh" && dfSource$uuid == "") {
+        hub_row <- dfSource[i,] %>%
+          mutate_if(is.numeric , replace_na, replace = 0) %>%
+          mutate_if(is.character , replace_na, replace = "") %>%
+          mutate_if(is.logical , replace_na, replace = 0)
+
+        clessnhub::edit_item(dfSource$uuid, as.list(hub_row), hubTableName)
+      }
+    }
+
+  } #for (i in i:nrow(dfSource))
+
+  return(dfDestination)
+
 }
