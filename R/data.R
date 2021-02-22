02fcf3af89159b29bf54ae043a51992aea5f25c7
PatrickPoncet <patrick@poncet.ca>
2021-02-22 07:31:09 -0500

    initial commit

---
+++ R/data.R
@@ -0,0 +1,140 @@
+######################################################
+#' @title clessnverse::createSimple
+#' @description
+#' @param
+#' @return
+#' @examples example
+#'
+#'
+#'
+#' @export
+createSimple <- function() {
+  return(data.frame(uuid = character(),
+                    created = character(),
+                    modified = character(),
+                    metedata = character(),
+                    eventID = character(),
+                    eventSourceType = character(),
+                    eventURL = character(),
+                    eventDate = character(),
+                    eventStartTime = character(),
+                    eventEndTime = character(),
+                    eventTitle = character(),
+                    eventSubtitle = character(),
+                    eventWordCount = character(),
+                    eventSentenceCount = character(),
+                    eventParagraphCount = integer(),
+                    eventContent = character(),
+                    eventTranslatedContent = character(),
+                    stringsAsFactors = FALSE))
+}
+
+
+######################################################
+#' @title clessnverse::createDeep
+#' @description
+#' @param
+#' @return
+#' @examples example
+#'
+#'
+#'
+#' @export
+createDeep <- function() {
+  return(data.frame(uuid = character(),
+                    created = character(),
+                    modified = character(),
+                    metedata = character(),
+                    eventID = character(),
+                    chapterNumber = character(),
+                    chapterTitle = character(),
+                    chapterTabledDocId = character(),
+                    chapterAdoptedDocId = character(),
+                    interventionSeqNum = integer(),
+                    speakerFirstName = character(),
+                    speakerLastName = character(),
+                    speakerFullName = character(),
+                    speakerGender = character(),
+                    speakerIsMinister = character(),
+                    speakerType = character(),
+                    speakerCountry = character(),
+                    speakerParty = character(),
+                    speakerPolGroup = character(),
+                    speakerDistrict = character(),
+                    speakerMedia = character(),
+                    speakerSpeechType = character(),
+                    speakerSpeechLang = character(),
+                    speakerSpeechWordCount = integer(),
+                    speakerSpeechSentenceCount = integer(),
+                    speakerSpeechParagraphCount = integer(),
+                    speakerSpeech = character(),
+                    speakerTranslatedSpeech = character(),
+                    stringsAsFactors = FALSE))
+}
+
+
+######################################################
+#' @title clessnverse::createCache
+#' @description
+#' @param
+#' @return
+#' @examples example
+#'
+#'
+#'
+#' @export
+createCache <- function() {
+  return(data.frame(uuid = character(),
+                    created = character(),
+                    modified = character(),
+                    metedata = character(),
+                    eventID = character(),
+                    eventHtml = character(),
+                    stringsAsFactors = FALSE))
+}
+
+
+######################################################
+#' @title clessnverse::getSimpleFromHub
+#' @description
+#' @param
+#' @return
+#' @examples example
+#'
+#'
+#'
+#' @export
+loadSimpleFromHub <- function() {
+  cat("1.1.4")
+}
+
+
+######################################################
+#' @title clessnverse::getDeepFromHub
+#' @description
+#' @param
+#' @return
+#' @examples example
+#'
+#'
+#'
+#' @export
+loadDeepFromHub <- function() {
+  cat("1.1.4")
+}
+
+
+######################################################
+#' @title clessnverse::getCacheFromHub
+#' @description
+#' @param
+#' @return
+#' @examples example
+#'
+#'
+#'
+#' @export
+loadCacheFromHub <- function() {
+  cat("1.1.4")
+}
+
