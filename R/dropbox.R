3dc495345c24e80e157dbc5ca2411f0e1557a6ec
PatrickPoncet <patrick@poncet.ca>
2021-03-12 16:47:58 -0500

    debug

--- R/dropbox.R
+++ R/dropbox.R
@@ -25,9 +25,10 @@ dbxListDir <- function(dir, token) {
     df <- data.frame(objectType=character(),
                      objectName=character(),
                      objectID=character())
+    print(r)
+    print(httr::content(r))
 
     for (i in 1:length(r$entries)) {
-
       df <- df %>% rbind(data.frame(objectType = r$entries[i][[1]]$.tag,
                                     objectName = r$entries[i][[1]]$path_lower,
                                     objectID = r$entries[i][[1]]$id))
