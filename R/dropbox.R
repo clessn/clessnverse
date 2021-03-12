0b193ffb25c20cedcd27bdbf8c6e9b75e191251c
PatrickPoncet <patrick@poncet.ca>
2021-03-12 17:50:21 -0500

    localpath fix

--- R/dropbox.R
+++ R/dropbox.R
@@ -163,7 +163,7 @@ dbxDownloadFile <- function(filename, local_path,token) {
                   httr::add_headers('Authorization' = paste("Bearer", token),
                                     'Dropbox-API-Arg'= header),
                   httr::progress(),
-                  httr::write_disk(local_path, TRUE))
+                  httr::write_disk(paste(local_path,filename,sep=''), TRUE))
 
   if (r$status_code == 200) {
 
