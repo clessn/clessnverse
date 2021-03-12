a110ab122a74f70520cd74fd9f8e465737e275dd
PatrickPoncet <patrick@poncet.ca>
2021-03-12 18:45:21 -0500

    ouf...

--- R/dropbox.R
+++ R/dropbox.R
@@ -33,7 +33,7 @@ dbxListDir <- function(dir, token) {
     for (i in 1:length(l$entries)) {
       df <- df %>% rbind(data.frame(objectType = l$entries[i][[1]]$.tag,
                                     objectName = basename(l$entries[i][[1]]$path_lower),
-                                    objectPath = dirname(l$entries[i][[1]]$path_lower))
+                                    objectPath = dirname(l$entries[i][[1]]$path_lower),
                                     objectID = l$entries[i][[1]]$id))
     }
 
