3e883ef31da37f3cb420d2528296454f7e0d186e
PatrickPoncet <patrick@poncet.ca>
2021-03-12 15:48:44 -0500

    ListDir

--- R/dropbox.R
+++ R/dropbox.R
@@ -21,13 +21,20 @@ dbxListDir <- function(dir, token) {
                   body = body,
                   encode = "form")
 
-  print(r)
-  print(httr::content(r))
-  return(httr::content(r))
-
   if (r$status_code == 200) {
+    df <- data.frame(objectType=character(),
+                     objectName=character(),
+                     objectID=character())
+
+    for (i in 1:length(r$entries)) {
+
+      df <- df %>% rbind(data.frame(objectType = r$entries[i][[1]]$.tag,
+                                    objectName = r$entries[i][[1]]$path_lower,
+                                    objectID = r$entries[i][[1]]$id))
+    }
+
     clessnverse::logit(paste("directory", dir, "sucessfully listed."), logger)
-    return(TRUE)
+    return(df)
   }
 }
 
