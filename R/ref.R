78fbf1655707c738309b0f472e2978efe0d525c8
PatrickPoncet <patrick@poncet.ca>
2021-07-21 14:16:29 -0400

    improvement of debate start time parsing

--- R/ref.R
+++ R/ref.R
@@ -186,7 +186,20 @@ loadETLRefData <- function(username, password, url) {
                            "^Présence d'ex-parlementaires de l'Assemblée nationale", "^Dépôt de documents (suite)",
                            "^Votes reportés", "^Réponse à une pétition", "^Réponse une pétition")
 
-  patterns_start_time_debate_fr <<- "^\\((une|deux|trois|quatre|cinq|six|sept|huit|neuf|dix|onze|douze|treize|quatorze|quinze|seize|dix-sept|dix-huit|dix-neuf|vingt|vingt-et-une|vingt-deux|vingt-trois)(.*)\\)$"
+  patterns_time_digits_or_text_fr <<- "^\\((une|1\\sheure|deux|2\\sheures|trois|3\\sheures|quatre|4\\sheures|cinq|5\\sheures|six|6\\sheures
+  |sept|7\\sheures|huit|8\\sheures|neuf|9\\sheures|dix|10\\sheures|onze|11\\sheures|douze|12\\sheures|treize|13\\sheures|quatorze|14\\sheures
+  |quinze|15\\sheures|seize|16\\sheures|dix-sept|17\\sheures|dix-huit|18\\sheures|dix-neuf|19\\sheures|vingt|20\\sheures|vingt-et-une|21\\sheures
+  |vingt-deux|22\\sheures|vingt-trois|23\\sheures)(.*)\\)$"
+
+  patterns_time_digits <<- "^\\((0(\\sheure|h)|1(\\sheure|h)|2(\\sheures|h)|3(\\sheures|h)|4(\\sheures|h)|
+  5(\\sheures|h)|6(\\sheures|h)|7(\\sheures|h)|8(\\sheures|h)|9(\\sheures|h)|10(\\sheures|h)|11(\\sheures|h)|
+  12(\\sheures|h)|13(\\sheures|h)|14(\\sheures|h)|15(\\sheures|h)|16(\\sheures|h)|17(\\sheures|h)|18(\\sheures|h)|
+  19(\\sheures|h)|20(\\sheures|h)|21(\\sheures|h)|22(\\sheures|h)|23(\\sheures|h)|)\\)$"
+
+  patterns_time_text_fr <<- "^\\((une(\\sheure|h)|deux(\\sheures|h)|trois(\\sheures|h)|quatre(\\sheures|h)|cinq(\\sheures|h)|six(\\sheures|h)
+  |sept(\\sheures|h)|huit(\\sheures|h)|neuf(\\sheures|h)|dix(\\sheures|h)|onze(\\sheures|h)|douze(\\sheures|h)|treize(\\sheures|h)|quatorze(\\sheures|h)
+  |quinze(\\sheures|h)|seize(\\sheures|h)|dix-sept(\\sheures|h)|dix-huit(\\sheures|h)|dix-neuf(\\sheures|h)|vingt(\\sheures|h)|vingt-et-une(\\sheures|h)
+  |vingt-deux(\\sheures|h)|vingt-trois(\\sheures|h))(.*)\\)$"
 
 
   # Connect to the HUB
