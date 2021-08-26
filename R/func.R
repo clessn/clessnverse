a895f6d88afb0265340933718fc609a9e9649092
Patrick Poncet <patrick@infoscope.ca>
2021-08-26 08:28:03 -0400

    fixed log

--- R/func.R
+++ R/func.R
@@ -73,7 +73,7 @@ logit <- function(scriptname="clessnverse", message = "", logger = NULL) {
       }
     },
     error = function(e) {
-      cat("console log: ",format(Sys.time(), "%Y-%m-%d %X"), "-", message, "\n")
+      cat("console log: ",format(Sys.time(), "%Y-%m-%d %X"), "-", paste(message, collapse = " "), "\n")
     }
   )
 }
