b6ff3cfe07a80c2a729d2592e364f2732db279ef
Patrick Poncet <patrick@infoscope.ca>
2021-12-23 07:55:20 -0500

    added text hours patterns (minuit, midi etc...)

--- R/etl.R
+++ R/etl.R
@@ -327,7 +327,7 @@ convertTextToNumberFR <- function(word) {
   wsplit <- wsplit[wsplit != ""]
   one_digits <- list(zéro=0, un=1, une = 1, deux=2, trois=3, quatre=4, cinq=5,
                      six=6, sept=7, huit=8, neuf=9)
-  teens <- list(onze=11, douze=12, treize=13, quatorze=14, quinze=15,
+  teens <- list(onze=11, douze=12, midi=12, treize=13, quatorze=14, quinze=15,
                 seize=16, "dix-sept"=17, "dix-huit"=18, "dix-neuf"=19, minuit=0)
   ten_digits <- list(dix=10, dipex=10, vingt=20, trente=30, quarante=40, cinquante=50,
                      soixante=60, "soixante-dix"=70, "quatre-vingts"=80, "quatre-vingt-dix"=90)
