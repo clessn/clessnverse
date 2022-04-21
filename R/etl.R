503b127a6a12654d9b7727fd2beb0987338a378e
Patrick Poncet <72944649+PatrickPoncet@users.noreply.github.com>
2022-04-20 21:42:49 -0400

    Added rm_accent

--- R/etl.R
+++ R/etl.R
@@ -375,6 +375,55 @@ convertTextToNumberFR <- function(word) {
 
 
 ######################################################
+#' @title clessnverse::rm_accents
+#' @description
+#' @param
+#' @param
+#' @param
+#' @return
+#' @examples
+#'
+#'
+#' @export
+rm_accent <- function(str,pattern="all") {
+  if(!is.character(str))
+    str <- as.character(str)
+  
+  pattern <- unique(pattern)
+  
+  if(any(pattern=="Ç"))
+    pattern[pattern=="Ç"] <- "ç"
+  
+  symbols <- c(
+    acute = "áéíóúÁÉÍÓÚýÝ",
+    grave = "àèìòùÀÈÌÒÙ",
+    circunflex = "âêîôûÂÊÎÔÛ",
+    tilde = "ãõÃÕñÑ",
+    umlaut = "äëïöüÄËÏÖÜÿ",
+    cedil = "çÇ"
+  )
+  
+  nudeSymbols <- c(
+    acute = "aeiouAEIOUyY",
+    grave = "aeiouAEIOU",
+    circunflex = "aeiouAEIOU",
+    tilde = "aoAOnN",
+    umlaut = "aeiouAEIOUy",
+    cedil = "cC"
+  )
+  
+  accentTypes <- c("´","`","^","~","¨","ç")
+  
+  if(any(c("all","al","a","todos","t","to","tod","todo")%in%pattern)) 
+    return(chartr(paste(symbols, collapse=""), paste(nudeSymbols, collapse=""), str))
+  
+  for(i in which(accentTypes%in%pattern))
+    str <- chartr(symbols[i],nudeSymbols[i], str) 
+  
+  return(str)
+} # </function rm_accent>
+
+######################################################
 ######################################################
 ######################################################
 #                     V1 Functions                   #
