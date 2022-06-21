9536e58b26ef2db393cd870995e4d625cbd57faf
Patrick Poncet <72944649+PatrickPoncet@users.noreply.github.com>
2022-06-21 12:59:23 -0230

    Update t.R

---
+++ R/t.R
@@ -0,0 +1,9 @@
+#compute_nb_sentences <- function(txt_bloc)
+#compute_nb_words <- function(txt_bloc)
+#compute_relevance_score <- function(txt_bloc, dictionary)
+#compute_sentiment_score <- function(txt_bloc, sentiment_dictionary)
+#compute_catergory_sentiment_score <- function(txt_bloc, category_dictionary, sentiment_dictionary)
+
+
+
+
