22353bacedfd91f0c757e3f213e7761104d69502
Patrick Poncet <72944649+PatrickPoncet@users.noreply.github.com>
2022-07-08 22:53:01 -0400

    upate

--- R/dev_t.R
+++ R/dev_t.R
@@ -140,7 +140,7 @@ get_hub2_table <- function(table_name, hubr_filter=list(), max_pages=-1, hub_con
     }
 
     path <- strsplit(path, "science")[[1]][[2]]
-    response <- http_post(path, body=hubr_filter, hub_config)
+    response <- http_post(path, body=hubr_filter, hub_conf)
     page <- httr::content(response)
   }
 
