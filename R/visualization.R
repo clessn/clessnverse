99d143e001b8908bd2e502525f66b8ebaf9b3b76
Judith Bourque <ejb7142@umoncton.ca>
2022-08-25 15:23:57 -0400

    + notes

--- R/visualization.R
+++ R/visualization.R
@@ -9,6 +9,7 @@
 # - theme_classic <3
 # - theme_minimal <3
 # - hrbrthemes::theme_ipsum()
+# Tutorial: https://statisticsglobe.com/ggplot2-themes-r
 
 theme_elxn_qc2022_light <- function(..., base_size = 12) {
   # Base theme
@@ -40,4 +41,20 @@ theme_elxn_qc2022_dark <- function(..., base_size = 12) {
       plot.title = element_text(face = "bold", hjust = 0)
     )
 }
+
+# Arguments
+
+# - mode: dark vs light
+# - project: election-qc2022
+
+
+# QUadrillé: default, none, when quadrillé, major lines only
+# Pas de barre en bas
+# CLESSN logo gris pale (voir github add logo symbol, only logo, droite)
+# Source gauche italique
+# axis text on major line?
+# Dimensions: twitter (16:9), facebook (2:3)
+# Make a table theme?
+# Pas de métho
+# Background: gris foncé et pâle
 
