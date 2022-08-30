468de3141fadfa0882dc7a6aa8426ff7d6ec9d64
Judith Bourque <ejb7142@umoncton.ca>
2022-08-29 22:58:33 -0400

    + datagotchi theme draft, clean code

--- R/visualization.R
+++ R/visualization.R
@@ -4,15 +4,16 @@
 #################################################################################################################
 #################################################################################################################
 
-# Light theme elxn-qc2022 ----
-# Interesting base themes
-# - theme_classic <3
-# - theme_minimal <3
-# - hrbrthemes::theme_ipsum()
+#' CLESSN themes
+#'
+#' These are themes that correspond to the CLESSN's branding and related
+#' projects.
+#'
+# hrbrthemes::theme_ipsum()
 # Tutorial: https://statisticsglobe.com/ggplot2-themes-r
 
 theme_classic_light <- function(base_size = 12) {
-  sysfonts::font_add_google("Schoolbell", "bell")
+  sysfonts::font_add_google("Roboto", "roboto")
   showtext::showtext_auto()
   # Base theme
   ggplot2::theme_classic() +
@@ -23,34 +24,20 @@ theme_classic_light <- function(base_size = 12) {
       legend.background = ggplot2::element_rect(fill = NA),
       # Align caption with left
       plot.caption = ggplot2::element_text(hjust = 0),
-      plot.title = ggplot2::element_text(family = "bell", face = "bold", size = base_size + 8, hjust = 0),
+      plot.title = ggplot2::element_text(face = "bold", size = base_size + 10, hjust = 0),
       plot.background = ggplot2::element_rect(fill = "#F8F8F8"),  # light, dark = #494949
       panel.background = ggplot2::element_rect(fill = NA)
     )
 }
 
-## Example ----
-
-ggplot2::ggplot(data = ggplot2::mpg) +
-  ggplot2::geom_point(mapping = ggplot2::aes(x = displ, y = hwy, colour = manufacturer)) +
-  ggplot2::labs(
-    title = "J'ai trouvé comment modifier les fonts lol",
-    subtitle = "Subtitle",
-    caption = "Source: ggplot2::mpg"
-  ) +
-  ggplot2::xlab("x axis label") +
-  ggplot2::ylab("y axis label") +
-  # Custom theme
-  theme_classic_light()
-
-# Dark theme elxn-qc2022 ----
-
-theme_classic_dark <- function(..., base_size = 12) {
+theme_classic_dark <- function(base_size = 12) {
+  sysfonts::font_add_google("Roboto", "roboto")
+  showtext::showtext_auto()
   # Base theme
   ggplot2::theme_classic() +
     # Changes to apply to base theme
     ggplot2::theme(
-      text = ggplot2::element_text(colour = "white"),
+      text = ggplot2::element_text(family = "roboto", colour = "white"),
       axis.text = ggplot2::element_text(colour = "white"),
       axis.ticks = ggplot2::element_line(colour = "white"),
       axis.line = ggplot2::element_line(colour = "white"),
@@ -65,19 +52,37 @@ theme_classic_dark <- function(..., base_size = 12) {
     )
 }
 
+theme_datagotchi <- function(base_size = 12){
+  sysfonts::font_add_google("VT323", "vt323")
+  showtext::showtext_auto()
+  # Base theme
+  ggplot2::theme_classic() +
+    # Changes to apply to base theme
+    ggplot2::theme(
+      legend.position = "bottom",
+      legend.title = ggplot2::element_blank(),
+      legend.background = ggplot2::element_rect(fill = NA),
+      # Align caption with left
+      plot.caption = ggplot2::element_text(hjust = 0),
+      plot.title = ggplot2::element_text(family = "vt323", size = base_size + 8, hjust = 0),
+      plot.background = ggplot2::element_rect(fill = "#9ebaa5"), # Not actual colour
+      panel.background = ggplot2::element_rect(fill = NA)
+    )
+}
+
 ## Example ----
 
 ggplot2::ggplot(data = ggplot2::mpg) +
   ggplot2::geom_point(mapping = ggplot2::aes(x = displ, y = hwy, colour = manufacturer)) +
   ggplot2::labs(
-    title = "Title",
-    subtitle = "Subtitle",
+    title = "Un très très beau graphique informatif",
+    subtitle = "Un sous-titre explicatif qui en dit long sur ces données importantes",
     caption = "Source: ggplot2::mpg"
   ) +
   ggplot2::xlab("x axis label") +
   ggplot2::ylab("y axis label") +
   # Custom theme
-  theme_classic_dark()
+  theme_datagotchi()
 
 # Load image
 # logo <- magick::image_read("icons/clessn_light.png")
