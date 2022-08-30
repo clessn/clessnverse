c7115857bd23bc92df7aedb52e457aa578f6cbf7
Judith Bourque <ejb7142@umoncton.ca>
2022-08-29 20:44:00 -0400

    customize fonts

--- R/visualization.R
+++ R/visualization.R
@@ -12,6 +12,8 @@
 # Tutorial: https://statisticsglobe.com/ggplot2-themes-r
 
 theme_classic_light <- function(base_size = 12) {
+  sysfonts::font_add_google("Schoolbell", "bell")
+  showtext::showtext_auto()
   # Base theme
   ggplot2::theme_classic() +
     # Changes to apply to base theme
@@ -21,7 +23,7 @@ theme_classic_light <- function(base_size = 12) {
       legend.background = ggplot2::element_rect(fill = NA),
       # Align caption with left
       plot.caption = ggplot2::element_text(hjust = 0),
-      plot.title = ggplot2::element_text(face = "bold", size = base_size + 8, hjust = 0),
+      plot.title = ggplot2::element_text(family = "bell", face = "bold", size = base_size + 8, hjust = 0),
       plot.background = ggplot2::element_rect(fill = "#F8F8F8"),  # light, dark = #494949
       panel.background = ggplot2::element_rect(fill = NA)
     )
@@ -32,7 +34,7 @@ theme_classic_light <- function(base_size = 12) {
 ggplot2::ggplot(data = ggplot2::mpg) +
   ggplot2::geom_point(mapping = ggplot2::aes(x = displ, y = hwy, colour = manufacturer)) +
   ggplot2::labs(
-    title = "Title",
+    title = "J'ai trouvé comment modifier les fonts lol",
     subtitle = "Subtitle",
     caption = "Source: ggplot2::mpg"
   ) +
