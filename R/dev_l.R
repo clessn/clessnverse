9af487052da9e10ca4bf081892297dbbfe4540fd
Patrick Poncet <72944649+PatrickPoncet@users.noreply.github.com>
2022-09-08 10:58:16 -0400

    Update dev_l.R

--- R/dev_l.R
+++ R/dev_l.R
@@ -92,59 +92,65 @@ commit_warehouse_row <- function(table, key, row = list(), mode = "refresh", cre
 #' @return return
 #' @examples # To be documented
 #' @export
-commit_warehouse_table <- function(table_name, df, key_column, key_encoding, non_null_constraint, refresh_data, credentials) {
+commit_warehouse_table <- function(table_name, df, key_columns, key_encoding, refresh_data, credentials) {
   my_table <- paste("clhub_tables_warehouse_", table_name, sep = "")
 
   # Check if table exists
   table_check <- hublot::filter_tables(credentials, list(verbose_name = paste("warehouse_",table_name,sep="")))
   if (length(table_check$results) == 0) stop(paste("The warehouse table specified in clessnverse::commit_warehouse_table() does not exist:", table_name))
 
-  if (stringr::str_detect(key_column, ",")) {
-  key_column <- gsub(" ", "", key_column)
-  key_column <- strsplit(key_column, ",")
-  key_column_mode <- "one"
+  if (stringr::str_detect(key_columns, ",")) {
+  key_columns <- gsub(" ", "", key_columns)
+  key_columns <- strsplit(key_columns, ",")
+  key_columns_mode <- "one"
   }
 
-  if (stringr::str_detect(key_column, "\\+")) {
-  key_column <- gsub(" ", "", key_column)
-  key_column <- strsplit(key_column, "\\+")
-  key_column_mode <- "combined"
+  if (stringr::str_detect(key_columns, "\\+")) {
+  key_columns <- gsub(" ", "", key_columns)
+  key_columns <- strsplit(key_columns, "\\+")
+  key_columns_mode <- "combined"
   } 
 
   # Compute the key
   key <- NULL
   i <- 1
-  if (key_column_mode == "one") {
-    while (NA %in% df[[key_column[[1]][i]]] || "" %in% df[[key_column[[1]][i]]]) {
+  if (key_columns_mode == "one") {
+    while (NA %in% df[[key_columns[[1]][i]]] || "" %in% df[[key_columns[[1]][i]]]) {
       i <- i + 1
     }
     
-    if (i <= length(key_column[[1]])) {
-      key <- df[[key_column[[1]][i]]]
+    if (i <= length(key_columns[[1]])) {
+      key <- df[[key_columns[[1]][i]]]
     } else {
-      clessnverse::logit(scriptname, paste("none of the key_columns specified is totally filled with non NA or non empty values: ", paste(key_column[[1]], collapse = " ")), logger)
-      stop("none of the key_columns specified is totally filled with non NA or non empty values: ", paste(key_column[[1]], collapse = " "))
+      clessnverse::logit(scriptname, paste("none of the key_columns specified is totally filled with non NA or non empty values: ", paste(key_columns[[1]], collapse = " ")), logger)
+      stop("none of the key_columns specified is totally filled with non NA or non empty values: ", paste(key_columns[[1]], collapse = " "))
     }
   }
 
-  if (key_column_mode == "combined") {
+  if (key_columns_mode == "combined") {
     key <- rep("", 1, nrow(df))
-    for (i in 1:length(key_column[[1]])) {
-      if (is.null(df[[key_column[[1]][i]]])) {
-        clessnverse::logit(scriptname, paste("column",  key_column[[1]][i], "does not exist in dataframe in function clessnverse::commit_warehouse_table()"), logger)
-        stop(paste("column",  key_column[[1]][i], "does not exist in dataframe in function clessnverse::commit_warehouse_table()"))
+    for (i in 1:length(key_columns[[1]])) {
+      if (is.null(df[[key_columns[[1]][i]]])) {
+        clessnverse::logit(scriptname, paste("column",  key_columns[[1]][i], "does not exist in dataframe in function clessnverse::commit_warehouse_table()"), logger)
+        stop(paste("column",  key_columns[[1]][i], "does not exist in dataframe in function clessnverse::commit_warehouse_table()"))
       }
 
-      if (NA %in% df[[key_column[[1]][i]]] || "" %in% df[[key_column[[1]][i]]]) {
-        clessnverse::logit(scriptname, paste("column",  key_column[[1]][i], "contains NA or empty values and cannot be a key in function commit_warehouse_table "), logger)
-        stop(paste("column",  key_column[[1]][i], "contains NA or empty values and cannot be a key in function commit_warehouse_table "))
+      if (NA %in% df[[key_columns[[1]][i]]] || "" %in% df[[key_columns[[1]][i]]]) {
+        clessnverse::logit(scriptname, paste("column",  key_columns[[1]][i], "contains NA or empty values and cannot be a key in function commit_warehouse_table "), logger)
+        stop(paste("column",  key_columns[[1]][i], "contains NA or empty values and cannot be a key in function commit_warehouse_table "))
       }
 
-      key <- paste(key, df[[key_column[[1]][i]]], sep = "")
+      key <- paste(key, df[[key_columns[[1]][i]]], sep = "")
     }
   }
 
   if (key_encoding == "digest") key <- unlist(lapply(X = as.list(key), FUN = digest::digest))
+
+  if (TRUE %in% duplicated(key)) {
+    clessnverse::logit(scriptname, paste("there are duplicated values in the unique key composed of the key_columns passed to clessnverse::commit_warehouse_table:", paste(key_columns, collapse = ",")), logger)
+    clessnverse::logit(scriptname, paste("The positions of duplicated keys in the dataframe are", paste(which(duplicated(key) == TRUE), collapse = ",")), logger)    
+    stop("duplicated keys exist in the dataframe passed to clessnverse::commit_warehouse_table")
+  }
 
   #json_combo <- df %>% mutate(json_combo = purrr::pmap(., ~jsonlite::toJSON(list(...),auto_unbox = T))) %>% select(json_combo)
   data <- df %>% mutate(data = purrr::pmap(., ~as.list(list(...)))) 
@@ -154,5 +160,5 @@ commit_warehouse_table <- function(table_name, df, key_column, key_encoding, non
 
   my_list <- do.call("mapply", c(list, new_df, SIMPLIFY = FALSE, USE.NAMES=FALSE))
 
-  hublot::batch_create_table_items("clhub_tables_warehouse_test_batch_load", my_list, credentials)
+  hublot::batch_create_table_items(my_table, my_list, credentials)
 }
