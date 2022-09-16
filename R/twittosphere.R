83ae2145e7ac26d76cbe62b1d9d986fe87579d11
Judith Bourque <ejb7142@umoncton.ca>
2022-09-16 17:22:31 -0400

    add example and author

--- R/twittosphere.R
+++ R/twittosphere.R
@@ -1,14 +1,17 @@
 #' Make a twittosphere
 #'
-#' Functions used to generate data to make a twittosphere
+#' Functions used to generate data to make a twittosphere.
 #'
 #' @param type 'journalist' or 'media'.
 #' @return A dataframe containing twitter accounts infos.
 #' @name twittosphere
 #' @importFrom clessnhub create_filter
 #' @importFrom clessnhub get_items
+#' @importFrom clessnhub login
 NULL
+#' @author Hubert Cadieux
 #' @examples
+#' clessnhub::login(Sys.getenv("HUB2_USERNAME"), Sys.getenv("HUB2_PASSWORD"))
 #' get_twitter_accounts(type = "journalist")
 #' get_twitter_accounts(type = "media")
 #' @export
