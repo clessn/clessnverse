6f5c9156e93afac448dd0239a4e5aab7db2a6249
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-03-16 15:25:47 -0400

    Create and document normalize_min_max() Also, render documentation for misc functions

--- R/domestication.R
+++ R/domestication.R
@@ -1,5 +1,27 @@
-#################################################################################################################
-#################################################################################################################
-########################################### Domestication Functions #############################################
-#################################################################################################################
-#################################################################################################################
+# Domestication
+
+#' Normalize a continuous variable between 0 and 1
+#'
+#' @param vector Numeric vector.
+#'
+#' @return Numeric vector.
+#' @export
+#'
+#' @examples
+#' data <- tibble::tibble(a = c(2, 0, 0, 0), b = c(4, 0, 0, 0))
+#'
+#' # Base R
+#' data_output <- sapply(data, normalize_min_max)
+#'
+#' # Dplyr
+#' library("dplyr")
+#'
+#' data_output <- data %>%
+#'   mutate(across(c(a, b), normalize_min_max))
+
+normalize_min_max <- function(x, remove_na = T) {
+  min <- min(x, na.rm = remove_na)
+  max <- max(x, na.rm = remove_na)
+  output <- (x - min )/(max - min)
+  return(output)
+}
