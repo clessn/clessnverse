69f7c396dcd01209f45a912e6c875ad940ad1d2f
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-03-16 15:31:56 -0400

    Simplify test

--- tests/testthat/test-domestication.R
+++ tests/testthat/test-domestication.R
@@ -1,7 +1,3 @@
-data <- tibble::tibble(a = c(2, 0, 0, 0), b = c(0, 0, 4, 0))
-
-data_output <- tibble::tibble(a = c(1, 0, 0, 0), b = c(0, 0, 1, 0))
-
 test_that("normalization works", {
-  expect_equal(dplyr::mutate(data, dplyr::across(c(a, b), normalize_min_max)), data_output)
+  expect_equal(normalize_min_max(c(4, 0, 2, 0)), c(1, 0, 0.5, 0))
 })
