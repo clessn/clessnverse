d199482f43d66cb3b515c5d7b7df9b96c5f35820
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-03-22 11:30:08 -0400

    Add test

--- tests/testthat/test-domestication.R
+++ tests/testthat/test-domestication.R
@@ -1,3 +1,7 @@
 test_that("normalization works", {
   expect_equal(normalize_min_max(c(4, 0, 2, 0)), c(1, 0, 0.5, 0))
 })
+
+test_that("reducing outliers works", {
+  expect_equal(reduce_outliers(c(5, 4, 6, -24, 5, 10, 2, 0, 2, 3, 32)), c(5, 4, 6, -3.25, 5, 10, 2, 0, 2, 3, 10.75))
+})
