48fe1f2e2f4aa58b2c8ed352e3213fec8805e30c
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-04 11:25:40 -0400

    Add RCurl dependencies

--- R/clessnverse-package.R
+++ R/clessnverse-package.R
@@ -4,5 +4,7 @@
 ## usethis namespace: start
 #' @importFrom fastText language_identification
 #' @importFrom lifecycle deprecated
+#' @importFrom RCurl curlEscape
+#' @importFrom RCurl getURL
 ## usethis namespace: end
 NULL
