198370752ff4e8ebb14399e9d1ba65350e24b70b
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-07 17:17:18 -0400

    Remove extra parenthesis

--- tests/testthat/test-dev_t.R
+++ tests/testthat/test-dev_t.R
@@ -1,14 +1,20 @@
 test_that("removing accents works", {
-  expect_equal(rm_accents(c("áćéíńóśúÁĆÉÍŃÓŚÚýÝźŹ")), "aceinosuACEINOSUyYzZ")  # Acute
-  expect_equal(rm_accents(c("àèìòùÀÈÌÒÙ")), "aeiouAEIOU")  # grave
-  expect_equal(rm_accents(c("âêîôûÂÊÎÔÛ")), "aeiouAEIOU")  # circunflex
-  expect_equal(rm_accents(c("ãõÃÕñÑ")), "aoAOnN")  # tilde
-  expect_equal(rm_accents(c("äëïöőüÄËÏÖŐÜÿ")), "aeioouAEIOOUy")  # umlaut
-  expect_equal(rm_accents(c("ąĄçÇęĘņŅțȚșȘşŞßßţŢ")), "aAcCenNEtTsSsSSStT")  # cedil
-  expect_equal(rm_accents(c("ăĂďĎěĚĕĔňŇšŠčČřŘžŽĕĔ")), "aAdDeEeEnNsScCrRzZeE")  # flex
-  expect_equal(rm_accents(c("ėĖżŻ")), "eEzZ")  # dotted
-  expect_equal(rm_accents(c("åÅ")), "aA")  # round
-  expect_equal(rm_accents(c("āĀēĒīĪūŪ")), "aAeEiIuU")  # bar
-  expect_equal(rm_accents(c("æÆ")), "aA")  # interlaced
-  expect_equal(rm_accents(c("łŁøØđĐ")), "lLoOdD")  # cross
+  # Removing accents without parameters
+  expect_equal(rm_accents("áćéíńóśúÁĆÉÍŃÓŚÚýÝźŹ"), "aceinosuACEINOSUyYzZ")  # Acute
+  expect_equal(rm_accents("àèìòùÀÈÌÒÙ"), "aeiouAEIOU")  # grave
+  expect_equal(rm_accents("âêîôûÂÊÎÔÛ"), "aeiouAEIOU")  # circunflex
+  expect_equal(rm_accents("ãõÃÕñÑ"), "aoAOnN")  # tilde
+  expect_equal(rm_accents("äëïöőüÄËÏÖŐÜÿ"), "aeioouAEIOOUy")  # umlaut
+  expect_equal(rm_accents("ąĄçÇęĘņŅțȚșȘşŞßßţŢ"), "aAcCenNEtTsSsSSStT")  # cedil
+  expect_equal(rm_accents("ăĂďĎěĚĕĔňŇšŠčČřŘžŽĕĔ"), "aAdDeEeEnNsScCrRzZeE")  # flex
+  expect_equal(rm_accents("ėĖżŻ"), "eEzZ")  # dotted
+  expect_equal(rm_accents("åÅ"), "aA")  # round
+  expect_equal(rm_accents("āĀēĒīĪūŪ"), "aAeEiIuU")  # bar
+  expect_equal(rm_accents("æÆ"), "aA")  # interlaced
+  expect_equal(rm_accents("łŁøØđĐ"), "lLoOdD")  # cross
+
+  # Removing one specicic accent
+
+  #expect_equal(rm_accents(), "lLoOdD")  # cross
+
 })
