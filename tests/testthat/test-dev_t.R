9c6e260f14363140e0365f14e4bd5ebaac496590
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-07 17:30:45 -0400

    Create tests for `rm_accents()` (#105)
    * Add tests for removing all accents
    
    * Add tests for removing one accent type

---
+++ tests/testthat/test-dev_t.R
@@ -0,0 +1,29 @@
+test_that("removing accents works", {
+
+  # Removing accents without parameters
+
+  expect_equal(rm_accents("áćéíńóśúÁĆÉÍŃÓŚÚýÝźŹ"), "aceinosuACEINOSUyYzZ")  # Acute
+  expect_equal(rm_accents("àèìòùÀÈÌÒÙ"), "aeiouAEIOU")  # grave
+  expect_equal(rm_accents("âêîôûÂÊÎÔÛ"), "aeiouAEIOU")  # circunflex
+  expect_equal(rm_accents("ãõÃÕñÑ"), "aoAOnN")  # tilde
+  expect_equal(rm_accents("äëïöőüÄËÏÖŐÜÿ"), "aeioouAEIOOUy")  # umlaut
+  expect_equal(rm_accents("ąĄçÇęĘņŅțȚșȘşŞßßţŢ"), "aAcCenNEtTsSsSSStT")  # cedil
+  expect_equal(rm_accents("ăĂďĎěĚĕĔňŇšŠčČřŘžŽĕĔ"), "aAdDeEeEnNsScCrRzZeE")  # flex
+  expect_equal(rm_accents("ėĖżŻ"), "eEzZ")  # dotted
+  expect_equal(rm_accents("åÅ"), "aA")  # round
+  expect_equal(rm_accents("āĀēĒīĪūŪ"), "aAeEiIuU")  # bar
+  expect_equal(rm_accents("æÆ"), "aA")  # interlaced
+  expect_equal(rm_accents("łŁøØđĐ"), "lLoOdD")  # cross
+
+  # Remove one accent type
+
+  str <- "áèîõüçę"
+
+  expect_equal(rm_accents(str, pattern = "´"), "aèîõüçę")
+  expect_equal(rm_accents(str, pattern = "`"), "áeîõüçę")
+  expect_equal(rm_accents(str, pattern = "^"), "áèiõüçę")
+  expect_equal(rm_accents(str, pattern = "~"), "áèîoüçę")
+  expect_equal(rm_accents(str, pattern = "¨"), "áèîõuçę")
+  expect_equal(rm_accents(str, pattern = "ç"), "áèîõüce")
+  expect_equal(rm_accents(str, pattern = ""), "áèîõüçę")
+})
