9816334eb4286472a3cba7ba3b3ddf5d5267f7bd
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-07 17:24:25 -0400

    Add tests for one accent type

--- tests/testthat/test-dev_t.R
+++ tests/testthat/test-dev_t.R
@@ -1,5 +1,7 @@
 test_that("removing accents works", {
+
   # Removing accents without parameters
+
   expect_equal(rm_accents("áćéíńóśúÁĆÉÍŃÓŚÚýÝźŹ"), "aceinosuACEINOSUyYzZ")  # Acute
   expect_equal(rm_accents("àèìòùÀÈÌÒÙ"), "aeiouAEIOU")  # grave
   expect_equal(rm_accents("âêîôûÂÊÎÔÛ"), "aeiouAEIOU")  # circunflex
@@ -13,8 +15,15 @@ test_that("removing accents works", {
   expect_equal(rm_accents("æÆ"), "aA")  # interlaced
   expect_equal(rm_accents("łŁøØđĐ"), "lLoOdD")  # cross
 
-  # Removing one specicic accent
+  # Remove one accent type
 
-  #expect_equal(rm_accents(), "lLoOdD")  # cross
+  str <- "áèîõüçę"
 
+  expect_equal(rm_accents(str, pattern = "´"), "aèîõüçę")
+  expect_equal(rm_accents(str, pattern = "`"), "áeîõüçę")
+  expect_equal(rm_accents(str, pattern = "^"), "áèiõüçę")
+  expect_equal(rm_accents(str, pattern = "~"), "áèîoüçę")
+  expect_equal(rm_accents(str, pattern = "¨"), "áèîõuçę")
+  expect_equal(rm_accents(str, pattern = "ç"), "áèîõüce")
+  expect_equal(rm_accents(str, pattern = ""), "áèîõüçę")
 })
