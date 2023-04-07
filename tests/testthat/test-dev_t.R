58b77191b5e2054a6f0a0a4587ed16c98f71622c
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-07 17:09:00 -0400

    Add tests for all accents

--- tests/testthat/test-dev_t.R
+++ tests/testthat/test-dev_t.R
@@ -1,3 +1,14 @@
 test_that("removing accents works", {
   expect_equal(rm_accents(c("áćéíńóśúÁĆÉÍŃÓŚÚýÝźŹ")), "aceinosuACEINOSUyYzZ")  # Acute
+  expect_equal(rm_accents(c("àèìòùÀÈÌÒÙ")), "aeiouAEIOU")  # grave
+  expect_equal(rm_accents(c("âêîôûÂÊÎÔÛ")), "aeiouAEIOU")  # circunflex
+  expect_equal(rm_accents(c("ãõÃÕñÑ")), "aoAOnN")  # tilde
+  expect_equal(rm_accents(c("äëïöőüÄËÏÖŐÜÿ")), "aeioouAEIOOUy")  # umlaut
+  expect_equal(rm_accents(c("ąĄçÇęĘņŅțȚșȘşŞßßţŢ")), "aAcCenNEtTsSsSSStT")  # cedil
+  expect_equal(rm_accents(c("ăĂďĎěĚĕĔňŇšŠčČřŘžŽĕĔ")), "aAdDeEeEnNsScCrRzZeE")  # flex
+  expect_equal(rm_accents(c("ėĖżŻ")), "eEzZ")  # dotted
+  expect_equal(rm_accents(c("åÅ")), "aA")  # round
+  expect_equal(rm_accents(c("āĀēĒīĪūŪ")), "aAeEiIuU")  # bar
+  expect_equal(rm_accents(c("æÆ")), "aA")  # interlaced
+  expect_equal(rm_accents(c("łŁøØđĐ")), "lLoOdD")  # cross
 })
