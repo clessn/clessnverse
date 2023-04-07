e2db16f0e249ca6aba8387c1c83bda5edd96a4c7
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-07 17:41:56 -0400

    Remove deprecated `testthat::context()` from tests (#106)
    * Remove context() for biased_samples()
    
    * Remove context() for clculate_proportions()
    
    * Remove context() for run_dictionary()

--- tests/testthat/test-analysis.R
+++ tests/testthat/test-analysis.R
@@ -12,9 +12,9 @@ library(crayon)
 
 #### 2. Sampling ####
 #### ~2.1 Creating multiple samples with probabilities biased by category ####
-context("Biased samples")
 
-test_that("`size` argument works", {
+test_that("Biased samples: `size` argument works", {
+  # Size argument
   expect_equal(object = length(sample_biased(data = CO2, x = Treatment, size = 1,
                                              probs = c(1, 1))$Treatment),
                expected = 1)
@@ -37,7 +37,7 @@ test_that("`size` argument works", {
                                         probs = c(1, 1))$Treatment)
 })
 
-test_that("`iterations` argument works", {
+test_that("Biased samples: `iterations` argument works", {
   expect_equal(object = length(sample_biased(data = CO2, x = Treatment, size = 4,
                                              iterations = 4,
                                              probs = c(1, 1))$Treatment),
@@ -58,7 +58,7 @@ test_that("`iterations` argument works", {
                                       iterations = -1, probs = c(1, 1))$Treatment)
 })
 
-test_that("`probs` argument works", {
+test_that("Biased samples: `probs` argument works", {
   expect_equal(object = length(sample_biased(data = CO2, x = Treatment, size = 4,
                                              probs = c(100, 0.5))$Treatment),
                expected = 4)
@@ -74,7 +74,7 @@ test_that("`probs` argument works", {
                                       probs = c(0.1, 0.04, 0.5))$Treatment)
 })
 
-test_that("`replace` argument works", {
+test_that("Biased samples: `replace` argument works", {
   expect_error(object = length(sample_biased(data = CO2, x = Treatment,
                                              size = length(CO2$Treatment) + 1,
                                              replace = FALSE,
@@ -89,8 +89,7 @@ test_that("`replace` argument works", {
 
 #### 3. Weighting ####
 #### ~3.1 Calc proportions of categories for one variable ####
-context("Calculate Proportions")
-test_that("`variable` argument works", {
+test_that("Calculate Proportions: `variable` argument works", {
   expect_equal(object = unlist(unname(c(
     round(calculate_proportions(data = CO2, variable = Plant)[1, 3], 4)
   ))),
@@ -114,7 +113,6 @@ test_that("`variable` argument works", {
 
 #### 4. Dictionary analysis ####
 #### ~4.1 Run dictionary ####
-context("Run dictionary analysis")
 FakeData <- data.frame(text = c(
   "I am not able to lie.", "I was abandoned.",
   paste0("My friend is an accomplished person. He was abandoned",
@@ -122,7 +120,7 @@ FakeData <- data.frame(text = c(
 FakeDictionary <- data.frame(negative = c("abandoned", "abolish"))
 ShortDictionary <- dictionary(list(negative = c("abandoned",
                                                 "abolish")))
-test_that("`dictionary` argument works", {
+test_that("Run dictionary: `dictionary` argument works", {
   expect_equal(object = class(run_dictionary(
     data = FakeData, text = text,
     dictionary = data_dictionary_LSD2015)),
@@ -143,7 +141,7 @@ test_that("`dictionary` argument works", {
     data = FakeData, text = tex,
     dictionary = data_dictionary_LSD2015)$negative)
 })
-test_that("`dictionary` argument works", {
+test_that("Run dictionary: `dictionary` argument works", {
   expect_error(object = run_dictionary(
     data = FakeData, text = text,
     dictionary = FakeDictionary)$negative)
