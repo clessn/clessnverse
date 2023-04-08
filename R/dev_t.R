f1ca0154085905ca5fa6af8431bec3345d40a246
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-07 20:33:00 -0400

    Add unicode grave

--- R/dev_t.R
+++ R/dev_t.R
@@ -1160,7 +1160,7 @@ rm_accents <- function(str,pattern="all") {
 
   symbols <- c(
     acute = "áćéíńóśúÁĆÉÍŃÓŚÚýÝźŹ",
-    grave = "àèìòùÀÈÌÒÙ",
+    grave = "\u00e0\u00e8\u00ec\u00f2\u00f9\u00c0\u00c8\u00cc\u00d2\u00d9",
     circunflex = "\u00e2\u00ea\u00ee\u00f4\u00fb\u00c2\u00ca\u00ce\u00d4\u00db",
     tilde = "\u00e3\u00f5\u00c3\u00d5\u00f1\u00d1",
     umlaut = "\u00e4\u00eb\u00ef\u00f6\u0151\u00fc\u00c4\u00cb\u00cf\u00d6\u0150\u00dc\u00ff",
