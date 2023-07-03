c29ed66ed7f98490ceed91c14624855c9a22e501
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-07-03 18:56:43 -0400

    Add message when loading package

--- R/clessnverse.R
+++ R/clessnverse.R
@@ -2,3 +2,10 @@
 #' @importFrom locateip locate_ip
 #' @importFrom wikirest get_most_viewed_per_country
 NULL
+
+# Show message when loading package
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("DISCLAIMER: As of July 2023, `clessnverse` is no longer under active development.
+
+To avoid breaking dependencies, the package remains available \"as is\" with no warranty of any kind.")
+}
